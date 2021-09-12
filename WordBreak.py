6993fe46a675f15cf996d8bd1f690fc6f5a42d3a
krupesh <krup_32@yahoo.com>
2021-09-11 23:08:28 -0400

    this is word break problem

---
+++ WordBreak.py
@@ -0,0 +1,37 @@
+'''
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+'''
+
+class WordBreak:
+
+    def wordBreak(self, s: str, wordDict) -> bool:
+        def wordBreakRecur(s: str, word_dict, start: int):
+            if start == len(s):
+                return True
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] in word_dict and wordBreakRecur(s, word_dict, end):
+                    return True
+            return False
+
+        return wordBreakRecur(s, set(wordDict), 0)
+
+
+p = WordBreak()
+s = "catanddog"
+wordDict = ["cats", "dog", "sand", "and", "cat"]
+assert (p.wordBreak(s, wordDict))
