8e8fceff881e2abe2ad4e8cb59b4fdbff5619800
krupesh <krup_32@yahoo.com>
2021-09-12 00:03:13 -0400

    basic common ancestor

---
+++ LowestCommonAncestorInTree.py
@@ -0,0 +1,27 @@
+class LowestCommonAncestorInTree:
+
+    def __init__(self):
+        # Variable to store LCA node.
+        self.ans = None
+
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        def recurse_tree(current_node):
+            # If reached the end of a branch, return False.
+            if not current_node:
+                return False
+
+            # Left Recursion
+            left = recurse_tree(current_node.left)
+
+            # Right Recursion
+            right = recurse_tree(current_node.right)
+
+            # If the current node is one of p or q
+            mid = current_node == p or current_node == q
