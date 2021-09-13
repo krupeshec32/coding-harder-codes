934c2a140499e93c9477d1e8db514553bef266da
krupesh <krup_32@yahoo.com>
2021-09-13 11:59:32 -0400

    find longest palidrom substring

---
+++ FindLongestPalindromicSubstring.py
@@ -0,0 +1,38 @@
+'''
+
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Input: s = "cbbd"
+Output: "bb"
+
+'''
+
+
+class FindLongestPalindromicSubstring:
+
+    def __init__(self, inputStr):
+        self.inputStr = inputStr
+
+    def getResult(self, inputStr):
+        result = {}
+        if len(inputStr) == 1:
+            # print(inputStr)
+            return inputStr
+        for i in range(0, len(inputStr)):
+            for j in range(i+1, len(inputStr)):
+                tempStr = inputStr[i:j]
+                reversedStr = tempStr[::-1]
+                if tempStr == reversedStr:
+                    result[len(tempStr)] = tempStr
+        print(result)
+        answer = sorted(result.items(), key=lambda x: x[0], reverse=True)
+        # print(answer[0][1])
+        return answer[0][1]
+
+
+inputStr = 'madamkrupeshdadpatel'
+x = FindLongestPalindromicSubstring(inputStr)
+result = x.getResult(inputStr)
+assert x.getResult(inputStr) == 'madam'
