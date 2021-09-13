3d655ad2b074554f73efd3a44fb6a50a36acf276
krupesh <krup_32@yahoo.com>
2021-09-13 12:32:54 -0400

    divide without using / or mod

---
+++ PerformDivideWithOutDivideORMod.py
@@ -0,0 +1,37 @@
+class PerformDivideWithOutDivideORMod:
+
+    def divide(self, dividend: int, divisor: int) -> int:
+
+        # Constants.
+        MAX_INT = 2147483647  # 2**31 - 1
+        MIN_INT = -2147483648  # -2**31
+
+        # Special case: overflow.
+        if dividend == MIN_INT and divisor == -1:
+            return MAX_INT
+
+        # We need to convert both numbers to negatives
+        # for the reasons explained above.
+        # Also, we count the number of negatives signs.
+        negatives = 2
+        if dividend > 0:
+            negatives -= 1
+            dividend = -dividend
+        if divisor > 0:
+            negatives -= 1
+            divisor = -divisor
+
+        # Count how many times the divisor has to be
+        # added to get the dividend. This is the quotient.
+        quotient = 0
+        while dividend - divisor <= 0:
+            quotient -= 1
+            dividend -= divisor
+
+        # If there was originally one negative sign, then
+        # the quotient remains negative. Otherwise, switch
+        # it to positive.
+        return -quotient if negatives != 1 else quotient
+
+x=PerformDivideWithOutDivideORMod()
+print(x.divide(2,7))
